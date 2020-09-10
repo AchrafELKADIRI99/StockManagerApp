c274747a3aa1e1c250f294b2dbb2014800037bd1
tarik9 <39895564+tarik9@users.noreply.github.com>
2020-09-10 17:37:03 +0100

    la connection avec la base de donnee

---
+++ src/com_connection/ConnectionDB.java
@@ -0,0 +1,23 @@
+package com_connection;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class ConnectionDB {
+	
+	public static Connection conDB() 
+    {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3308/biblio?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Africa/Casablanca", "root","");
+            return conn;
+        } catch (Exception  ex) {
+            System.err.println("erreur :"+ex.getMessage());
+            return null;
+        }
+    }
+
+}
